7053902010fc6a8d25b2e250a42653cbeb17b5c5
Paul Wu <easylive1989@gmail.com>
2017-07-27 23:42:20 +0800

    first commit

---
+++ app/src/main/java/com/paulps/wallpaperswitcher/ExampleAppWidgetProvider.java
@@ -0,0 +1,27 @@
+package com.paulps.wallpaperswitcher;
+
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class ExampleAppWidgetProvider extends AppWidgetProvider {
+
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            Intent intent = new Intent(context, WallpaperModifyService.class);
+            PendingIntent pendingIntent = PendingIntent.getService(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_main);
+            views.setOnClickPendingIntent(R.id.icon, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
---
+++ app/src/main/java/com/paulps/wallpaperswitcher/WallpaperModifyService.java
@@ -0,0 +1,60 @@
+package com.paulps.wallpaperswitcher;
+
+import android.app.IntentService;
+import android.app.WallpaperManager;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class WallpaperModifyService extends IntentService {
+    private static final String TAG = WallpaperModifyService.class.getSimpleName();
+
+    private static final String PREFERENCE_TABLE_GLOBAL = "PreferenceGlobal";
+    private static final String PREFERENCE_KEY_WP_RES = "WallpaperResource";
+
+    private final int DEFAULT_WP_ANIM = R.raw.default_wallpaper_anim;
+    private final int DEFAULT_WP_NORMAL = R.raw.default_wallpaper_normal;
+
+    private WallpaperManager mWallpaperManager;
+
+    public WallpaperModifyService() {
+        super(WallpaperModifyService.class.getSimpleName());
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        mWallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+        try {
+            if (isCurrentWpNormal()) {
+                mWallpaperManager.setResource(DEFAULT_WP_ANIM);
+                setCurrentWpRes(DEFAULT_WP_ANIM);
+            } else {
+                mWallpaperManager.setResource(DEFAULT_WP_NORMAL);
+                setCurrentWpRes(DEFAULT_WP_NORMAL);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isCurrentWpNormal() {
+        return getCurrentWpRes() == DEFAULT_WP_NORMAL;
+    }
+
+    private int getCurrentWpRes() {
+        SharedPreferences sp = getApplicationContext().getSharedPreferences(PREFERENCE_TABLE_GLOBAL, Context.MODE_PRIVATE);
+        return sp.getInt(PREFERENCE_KEY_WP_RES, 0);
+    }
+
+    private void setCurrentWpRes(int wpRes) {
+        SharedPreferences sp = getApplicationContext().getSharedPreferences(PREFERENCE_TABLE_GLOBAL, Context.MODE_PRIVATE);
+        sp.edit().putInt(PREFERENCE_KEY_WP_RES, wpRes).commit();
+    }
+}
